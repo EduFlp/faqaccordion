3bc75b1edbc833e6ec9c8d601bec57270f76cbe3
Eduardo Pereira <eduardofelipe940@gmail.com>
2024-06-17 23:01:24 -0300

    attempts

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const faqs = document.querySelectorAll(".faq");
+document.addEventListener('DOMContentLoaded', () =>{
+    const faqContainer = document.querySelector('.card');
+    
+    faqContainer.addEventListener('click', (e) =>{
+        const gruopHeader = e.target.closest('.questions');
 
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    })
-})
+        if(!gruopHeader) return;
+
+        const group = gruopHeader.parentElement;
+        const groupbody = group.querySelector('.faq-content');
+        const icon = gruopHeader.querySelector('.faq-icon');
+
+        icon.classList.toggle('open');
+        groupbody.classList.toggle('open');
+        
+        const otherGroups = faqContainer.querySelectorAll('.faq');
+
+        otherGroups.forEach((otherGroup)=>{
+            if(otherGroup !== group){
+                const otherGroupBody = otherGroup.querySelector(".faq-content");
+                const otherIcon = otherGroup.querySelector('.faq-icon');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('open');
+            }
+            
+            });  
+});
+});
